2cba1fef2bedd093311632028f86a0a2530b98ff
Mateusz Krawczyk <matixoni@o2.pl>
2017-03-27 10:27:07 +0200

    adding git ignore

---
+++ Enferia/Enferia/ViewModels/StartUpViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Caliburn.Micro;
+
+
+namespace Enferia.ViewModels
+{
+    class StartUpViewModel : Conductor<object>.Collection.OneActive 
+    {
+        private LoginPageViewModel log = new LoginPageViewModel();
+        private RegistrationPageViewModel reg = new RegistrationPageViewModel();
+        public StartUpViewModel()
+        {
+            ActivateItem(log);
+        }
+        
+        public void aboutPage_Button()
+        {
+            MessageBox.Show("ja to ja");
+        }
+
+        public void registryPage_Button()
+        {
+            ActivateItem(reg);
+        }
+        
+        public void loginPage_Button()
+        {
+            ActivateItem(log);
+        }
+   
+    }
+}
